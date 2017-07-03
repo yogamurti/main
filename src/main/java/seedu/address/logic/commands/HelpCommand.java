1370d59467854f6a92a8c8afe9e06e086436ac76
radneran <a0124399@u.nus.edu>
2017-07-03 19:35:32 +0800

    Initial Commit with correct checkstyle

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.commands;
 
-
 import seedu.address.commons.core.EventsCenter;
+
 import seedu.address.commons.events.ui.ShowHelpRequestEvent;
+
 
 /**
  * Format full help instructions for every command for display.
