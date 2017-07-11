4101338a34a71022302ecbc207a80f2acfd92161
Yogamurti Sutanto <yogamurtisutanto@gmail.com>
2017-07-11 16:19:43 +0800

    Fix TaskBookFilePathChangedEvent attempt.

--- src/main/java/teamthree/twodo/commons/events/storage/TaskBookFilePathChangedEvent.java
+++ src/main/java/teamthree/twodo/commons/events/storage/TaskBookFilePathChangedEvent.java
@@ -3,15 +3,16 @@ package teamthree.twodo.commons.events.storage;
 import teamthree.twodo.commons.events.BaseEvent;
 
 public class TaskBookFilePathChangedEvent extends BaseEvent {
-    
+
     public final String filePath;
-  
-  public TaskBookFilePathChangedEvent(String filePath) {
-      this.filePath = filePath;
-      }
-  
-  @Override
-  public String toString() {
-      return this.getClass().getSimpleName();
-      }
+
+    public TaskBookFilePathChangedEvent(String filePath) {
+        this.filePath = filePath;
+        }
+    
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+        }
+
 }
