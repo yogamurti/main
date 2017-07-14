ded3ddde9c5be36249fd0242afd130caceefa0ec
radneran <a0124399@u.nus.edu>
2017-07-14 21:42:05 +0800

    Add tests for Task

---
+++ src/test/java/teamthree/twodo/model/task/TaskTest.java
@@ -0,0 +1,39 @@
+package teamthree.twodo.model.task;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import teamthree.twodo.commons.exceptions.IllegalValueException;
+import teamthree.twodo.testutil.FloatingTaskBuilder;
+import teamthree.twodo.testutil.TaskWithDeadlineBuilder;
+
+public class TaskTest {
+
+    @Test
+    public void resetTaskSuccessfullyForFloating() {
+        try {
+            Task original = new FloatingTaskBuilder().build();
+            Task replacement = new FloatingTaskBuilder().withName("Replacement").build();
+            original.resetData(replacement);
+            assertTrue(original.equals(replacement));
+        } catch (IllegalValueException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void resetTaskSuccessfullyForDeadlinesAndCompletedTask() {
+        try {
+            Task original = new TaskWithDeadlineBuilder().build();
+            Task replacement = new TaskWithDeadlineBuilder().withName("Replacement").build();
+            replacement.markCompleted();
+            original.resetData(replacement);
+            assertTrue(original.equals(replacement));
+        } catch (IllegalValueException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
