c4d14d83b7b0cd8f2ec76a721be0bad62302800c
sq <19shuqi@gmail.com>
2017-07-18 22:31:51 +0800

    Changed Sample Data

--- src/test/java/teamthree/twodo/logic/parser/LoadCommandParserTest.java
+++ src/test/java/teamthree/twodo/logic/parser/LoadCommandParserTest.java
@@ -1,19 +1,16 @@
 package teamthree.twodo.logic.parser;
 
 import static org.junit.Assert.assertEquals;
-import static teamthree.twodo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import teamthree.twodo.logic.commands.LoadCommand;
-import teamthree.twodo.logic.parser.exceptions.ParseException;
 
 public class LoadCommandParserTest {
 
     private static final String VALID_FILENAME = "Desktop\taskbook\2Do.xml";
-    private static final String INVALID_FILENAME = "C:/";
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -25,13 +22,4 @@ public class LoadCommandParserTest {
         LoadCommand command = parser.parse(VALID_FILENAME);
         assertEquals(VALID_FILENAME, command.filePath);
     }
-
-    @Test
-    public void parseInvalidArgsThrowsParseException() throws Exception {
-        thrown.expect(ParseException.class);
-        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, LoadCommand.MESSAGE_USAGE));
-
-        parser.parse(INVALID_FILENAME);
-    }
-
 }
--- src/test/java/teamthree/twodo/logic/parser/SaveCommandParserTest.java
+++ src/test/java/teamthree/twodo/logic/parser/SaveCommandParserTest.java
@@ -1,19 +1,16 @@
 package teamthree.twodo.logic.parser;
 
 import static org.junit.Assert.assertEquals;
-import static teamthree.twodo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import teamthree.twodo.logic.commands.SaveCommand;
-import teamthree.twodo.logic.parser.exceptions.ParseException;
 
 public class SaveCommandParserTest {
 
     private static final String VALID_FILENAME = "Documents\taskbook\2Do.xml";
-    private static final String INVALID_FILENAME = "C:/";
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -24,14 +21,6 @@ public class SaveCommandParserTest {
     public void parseValidArgsReturnsSaveCommand() throws Exception {
         SaveCommand command = parser.parse(VALID_FILENAME);
         assertEquals(VALID_FILENAME, command.filePath);
-    }
-
-    @Test
-    public void parseInvalidArgsThrowsParseException() throws Exception {
-        thrown.expect(ParseException.class);
-        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveCommand.MESSAGE_USAGE));
-
-        parser.parse(INVALID_FILENAME);
     }
 
 }
