8cb584de0b135defb723f4f30648eecc0740287f
Yogamurti Sutanto <yogamurtisutanto@gmail.com>
2017-07-21 16:09:18 +0800

    Add UnmarkCommandParserTest

--- src/test/java/teamthree/twodo/logic/parser/UnmarkCommandParserTest.java
+++ src/test/java/teamthree/twodo/logic/parser/UnmarkCommandParserTest.java
@@ -1,5 +1,35 @@
 package teamthree.twodo.logic.parser;
 
+import static org.junit.Assert.assertEquals;
+import static teamthree.twodo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static teamthree.twodo.testutil.TypicalTask.INDEX_FIRST_TASK;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import teamthree.twodo.logic.commands.UnmarkCommand;
+import teamthree.twodo.logic.parser.exceptions.ParseException;
+
+//@@author A0139267W
 public class UnmarkCommandParserTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private UnmarkCommandParser parser = new UnmarkCommandParser();
+
+    @Test
+    public void parseValidArgsReturnsUnmarkCommand() throws Exception {
+        UnmarkCommand command = parser.parse("1");
+        assertEquals(INDEX_FIRST_TASK, command.targetIndex);
+    }
+
+    @Test
+    public void parseInvalidArgsThrowsParseException() throws Exception {
+        thrown.expect(ParseException.class);
+        thrown.expectMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkCommand.MESSAGE_USAGE));
+
+        parser.parse("a");
+    }
 }
