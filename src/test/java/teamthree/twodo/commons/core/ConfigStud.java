bbea8c2c6ca3259bec53014c1d8032f513afd265
Yogamurti Sutanto <yogamurtisutanto@gmail.com>
2017-07-22 13:42:57 +0800

    Add OptionsCommandTest

---
+++ src/test/java/teamthree/twodo/commons/core/ConfigStud.java
@@ -0,0 +1,118 @@
+package teamthree.twodo.commons.core;
+
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//@@author A0139267W-reused
+//A copy of Config to bypass static declarations for testing purposes
+public class ConfigStud {
+
+    // Default notification period of 1 day. Can be changed by user.
+    private static Long notificationPeriod = (long) (1000 * 60 * 60 * 24);
+    private static String notificationPeriodToString = "1 day";
+    private static String defaultConfigFile = "config.json";
+
+    // To change default notification period
+    private static final long DAY_TO_MILLIS = 1000 * 60 * 60 * 24;
+    private static final long WEEK_TO_MILLIS = DAY_TO_MILLIS * 7;
+
+    // Config values customizable through config file
+    private String appTitle = "2Do";
+    private Level logLevel = Level.INFO;
+    private String userPrefsFilePath = "preferences.json";
+    private String taskBookFilePath = "data/2Do.xml";
+
+    public static Long getDefaultNotificationPeriod() {
+        return notificationPeriod;
+    }
+
+    public static void setDefaultNotificationPeriod(long newNotificationPeriod) {
+        notificationPeriod = newNotificationPeriod;
+    }
+
+    public String getAppTitle() {
+        return appTitle;
+    }
+
+    public void setAppTitle(String appTitle) {
+        this.appTitle = appTitle;
+    }
+
+    public Level getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(Level logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    public String getUserPrefsFilePath() {
+        return userPrefsFilePath;
+    }
+
+    public void setUserPrefsFilePath(String userPrefsFilePath) {
+        this.userPrefsFilePath = userPrefsFilePath;
+    }
+
+    public void setTaskBookFilePath(String taskBookFilePath) {
+        this.taskBookFilePath = taskBookFilePath;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof Config)) { //this handles null as well.
+            return false;
+        }
+
+        ConfigStud o = (ConfigStud) other;
+
+        return Objects.equals(appTitle, o.appTitle) && Objects.equals(logLevel, o.logLevel)
+                && Objects.equals(userPrefsFilePath, o.userPrefsFilePath);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appTitle, logLevel, userPrefsFilePath);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("App title : " + appTitle);
+        sb.append("\nCurrent log level : " + logLevel);
+        sb.append("\nPreference file Location : " + userPrefsFilePath);
+        sb.append("\nTask Book file Location " + taskBookFilePath);
+        return sb.toString();
+    }
+
+    public static String getDefaultConfigFile() {
+        return defaultConfigFile;
+    }
+
+    public String getTaskBookFilePath() {
+        return taskBookFilePath;
+    }
+
+    public static void changeDefaultNotificationPeriod(String newNotificationPeriod) {
+        notificationPeriodToString = newNotificationPeriod;
+        Matcher integerParser = Pattern.compile("\\d*").matcher(newNotificationPeriod);
+        assert (integerParser.find());
+        int period = Integer.parseInt(integerParser.group().trim());
+        long newDefault = 0;
+        if (newNotificationPeriod.toLowerCase().contains("day")) {
+            newDefault = DAY_TO_MILLIS * period;
+        } else if (newNotificationPeriod.toLowerCase().contains("week")) {
+            newDefault = WEEK_TO_MILLIS * period;
+        }
+        notificationPeriod = newDefault;
+    }
+
+    public static String defaultNotificationPeriodToString() {
+        return notificationPeriodToString;
+    }
+}
